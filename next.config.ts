62e7a4bf3089ceaf9f149c7c95e0fd8bec04fb30
Mohamed-Osama17 <moohamedosama3@gmail.com>
2025-08-28 15:38:36 +0300

    Modifying the next config and package.json

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
-'https://linked-posts.routemisr.com/uploads/69d705b0-76b8-44a4-b43a-7353b49860be-IMG_3249.JPG'
+("https://linked-posts.routemisr.com/uploads/69d705b0-76b8-44a4-b43a-7353b49860be-IMG_3249.JPG");
+
+const isProd = process.env.NODE_ENV === "production";
 
 const config: NextConfig = {
+  reactStrictMode: true,
+  output: "export", // enables static HTML export
+  assetPrefix: isProd ? "/circle-app/" : "",
+  basePath: isProd ? "/circle-app" : "",
   images: {
     remotePatterns: [
       {
