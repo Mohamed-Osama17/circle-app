f5ca7cbee1dbcad321d442c857beb1d94ccb35fd
Mohamed-Osama17 <moohamedosama3@gmail.com>
2025-08-28 20:24:53 +0300

    modifying the next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 
-("https://linked-posts.routemisr.com/uploads/69d705b0-76b8-44a4-b43a-7353b49860be-IMG_3249.JPG");
-
-const isProd = process.env.NODE_ENV === "production";
-
 const config: NextConfig = {
-  reactStrictMode: true,
-  assetPrefix: isProd ? "/circle-app/" : "",
-  basePath: isProd ? "/circle-app" : "",
   images: {
     remotePatterns: [
       {
